c979d32daccbcd600ac147b1a61b58edabd2bfbb
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2021-07-07 16:33:36 -0500

    menuitem pseudocode done

---
+++ menuItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function MenuItem() {
+  return (
+    <div>
+      {/* 
+      Main Container
+        Product Image Container
+        ¿¿ Price ??
+        Product Description Container
+          Product Title
+          Product Description (ex. Ingredients)
+          ¿¿ Price ??
+        ¿¿ Price ??
+
+    */}
+    </div>
+  );
+}
+
+export default MenuItem;
