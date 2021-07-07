2761d3275c757098fa206b2f6c4d51b983817ce9
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2021-07-07 16:12:42 -0500

    Started pseudo, touch menuItem

--- mainMenu.js
+++ mainMenu.js
@@ -1,5 +1,18 @@
+import React from "react";
+
 function MainMenu() {
-  return <div>MainMenu</div>;
+  return (
+    <div>
+      {/* 
+      Main Cointainer
+        Top Bar 
+        Top Bowls Container 
+          Bowl1 Bowl 2
+        Signature Bowls 
+          MenuItem
+    */}
+    </div>
+  );
 }
 
 export default MainMenu;
